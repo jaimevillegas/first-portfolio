a04d8e6785729237f05e62c8250564d1ce3ea004
Jaime Villegas <jaimevillegas296@gmail.com>
2023-01-30 16:13:44 -0500

    Work on Modal window

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 // Get the modal
-var navMenu = document.getElementById("nav-menu");
+const navMenu = document.getElementById('nav-menu');
 
 // Get the button that opens the modal
-var btn = document.getElementById("menu-toggle");
+const btn = document.getElementById('menu-toggle');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
+
+const item1 = document.getElementById('item1');
+const item2 = document.getElementById('item2');
+const item3 = document.getElementById('item3');
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
-  navMenu.style.display = "block";
+  navMenu.style.display = 'block';
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  navMenu.style.display = "none";
+  navMenu.style.display = 'none';
 };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == navMenu) {
-    navMenu.style.display = "none";
-  }
+item1.onclick = function () {
+  navMenu.style.display = 'none';
+};
+
+item2.onclick = function () {
+  navMenu.style.display = 'none';
+};
+
+item3.onclick = function () {
+  navMenu.style.display = 'none';
 };
